75f6d58bb8b3268dbda14706bfa6dea4d0744e9f
hanafathiyah <13520047@std.stei.itb.ac.id>
2023-04-05 19:33:26 +0700

    add add matrix and multiply matrix

--- src/helper.py
+++ src/helper.py
@@ -21,3 +21,26 @@ def softmax(net_i, arr_net):
     for i in arr_net:
         net_sum += i
     return (exp(net_i))/net_sum
+
+
+def multiplyMatrix(m1, m2):
+    m3 = []
+    for i in range(len(m1)):
+        m3.append([])
+        for j in range(len(m2[0])):
+            m3[i].append(0.0)
+            for k in range(len(m1[0])):
+                m3[i][j] += float(m1[i][k] * m2[k][j])
+
+    return m3
+
+
+def addMatrix(m1, m2):
+    m3 = []
+    for i in range(len(m1)):
+        m3.append([])
+        for j in range(len(m2)):
+            m3[i].append(0)
+            m3[i][j] = m1[i][j] + m2[i][j]
+
+    return m3
