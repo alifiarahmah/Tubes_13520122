2c645eea553e8795622dd74b8bbd0fb027bcf8a2
hanafathiyah <13520047@std.stei.itb.ac.id>
2023-04-05 19:15:02 +0700

    add loop for every input

---
+++ src/helper.py
@@ -0,0 +1,23 @@
+# Helper function
+from math import exp
+
+# net -> persamaan linear (ax+b+...)
+
+
+def linear(net):
+    return net
+
+
+def relu(net):
+    return max(0, net)
+
+
+def sigmoid(net):
+    return float(1/(1 + exp(net * -1)))
+
+
+def softmax(net_i, arr_net):
+    net_sum = 0
+    for i in arr_net:
+        net_sum += i
+    return (exp(net_i))/net_sum
